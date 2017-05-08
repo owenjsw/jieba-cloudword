3f90ebf7038515acda63de90f6caae36203bf2b8
蒋世伟 <owen.jsw@icloud.com>
2017-05-08 18:41:08 +0800

    second commit

--- word.py
+++ word.py
@@ -11,39 +11,39 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 
 
-#
-# path1 = r'/Users/owen/Documents/tensor/static/result1.txt'
-#
-# jb = open(path1,'w+')
-# jieba.analyse.set_stop_words('/Users/owen/Desktop/stopwords.txt')
-# for line in open('/Users/owen/Desktop/query_result.csv'):
-#
-#     # item = line.strip('\n\r').split('\t')
-#     item = line.split(",")
-#     item2 = []
-#     # for i in item :
-#     #     print i
-#     #     if u'\u4e00' <= i <= u'\u9fff':
-#     #         item2.append(i)
-#
-#     # if item.encode('UTF-8').isalpha():
-#     #     continue
-#     # else:
-#     #     stra = ''.join(item)
-#     stra = ''.join(item)
-#
-#     tags = jieba.analyse.extract_tags(stra)
-#     tags2 = []
-#
-#     for tag in tags:
-#         if u'\u4e00' <= tag <= u'\u9fff':
-#             tags2.append(tag)
-#             print tag.encode('UTF-8')
-#     tagsw = ','.join(tags2)
-#     # tags = jieba.analyse.extract_tags(item[1])
-#     # tagsw = ",".join(tags)
-#     jb.write(tagsw)
-# jb.close()
+
+path1 = r'/Users/owen/Documents/tensor/static/result1.txt'
+
+jb = open(path1,'w+')
+jieba.analyse.set_stop_words('/Users/owen/Desktop/stopwords.txt')
+for line in open('/Users/owen/Desktop/query_result.csv'):
+
+    # item = line.strip('\n\r').split('\t')
+    item = line.split(",")
+    item2 = []
+    # for i in item :
+    #     print i
+    #     if u'\u4e00' <= i <= u'\u9fff':
+    #         item2.append(i)
+
+    # if item.encode('UTF-8').isalpha():
+    #     continue
+    # else:
+    #     stra = ''.join(item)
+    stra = ''.join(item)
+
+    tags = jieba.analyse.extract_tags(stra)
+    tags2 = []
+
+    for tag in tags:
+        if u'\u4e00' <= tag <= u'\u9fff':
+            tags2.append(tag)
+            print tag.encode('UTF-8')
+    tagsw = ','.join(tags2)
+    # tags = jieba.analyse.extract_tags(item[1])
+    # tagsw = ",".join(tags)
+    jb.write(tagsw)
+jb.close()
 
 d = path.dirname('.')
 back_coloring = imread(path.join(d, "./image/alice_mask.png"))
